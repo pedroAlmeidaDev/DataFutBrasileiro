d83f3d74a8543550b24cffbf7a0682b00348ffd9
pedroAlmeidaDev <p_henrique1182@hotmail.com>
2025-08-28 15:24:34 +0000

    Commit inicial do projeto

---
+++ Statisticas_Futebol_Brasileiro.R
@@ -0,0 +1,16 @@
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("tidyverse")
+library(tidyverse)
+
+library(readr)
+
+library(dplyr)
+library()
+
+campeonato_brasileiro_cartoes <- read.csv("campeonato-brasileiro-cartoes.csv")
+campeonato_brasileiro_estatisticas_full <- read.csv("campeonato-brasileiro-estatisticas-full.csv")
+campeonato_brasileiro_full <- read.csv("campeonato-brasileiro-full.csv")
+campeonato_brasileiro_gols <- read.csv("campeonato-brasileiro-gols.csv")
+
