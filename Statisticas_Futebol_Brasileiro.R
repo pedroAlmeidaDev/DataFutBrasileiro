3f963250cc2ab55597e2e80ad3cf023032bb4450
pedroAlmeidaDev <p_henrique1182@hotmail.com>
2025-08-31 02:31:18 +0000

    Média de gols por partida do Flamengo (2020–2024)

--- Statisticas_Futebol_Brasileiro.R
+++ Statisticas_Futebol_Brasileiro.R
@@ -10,6 +10,8 @@ library(readr)
 
 library(dplyr)
 
+library(lubridate)
+
 
 #Criando DataFrames a partir dos arquivos .CSV carregados 
 campeonato_brasileiro_cartoes <- read.csv("campeonato-brasileiro-cartoes.csv")
@@ -23,18 +25,18 @@ campeonato_brasileiro_full$data <- as.Date(campeonato_brasileiro_full$data, form
 #Média de gols por partida dos clubes em 2024
 MediaGolsMarcados <- campeonato_brasileiro_gols %>%
   inner_join(campeonato_brasileiro_full, by = c("partida_id" = "ID")) %>%
-  filter(format(data, "%Y") == "2024") %>%
+  filter(year(data) == 2024) %>%
   group_by(clube, partida_id) %>%
   summarize(gols_por_partida = n(), .groups = "drop") %>%
   group_by(clube) %>%
-  summarize(mean_gols = mean(gols_por_partida), .groups = "drop") %>%
+  summarize(mean_gols = round(mean(gols_por_partida),2), .groups = "drop") %>%
   arrange(desc(mean_gols))
 
 #Gráfico em barras da Média de gols por partida dos clubes em 2024
 ggplot(data = MediaGolsMarcados, aes(x = clube, y = mean_gols, fill = clube)) + 
   geom_bar(stat = "identity") +
   geom_text(
-    aes(label = round(mean_gols, 2)),
+    aes(label = mean_gols),
     vjust = -0.8
   ) + 
   labs(
@@ -45,4 +47,27 @@ ggplot(data = MediaGolsMarcados, aes(x = clube, y = mean_gols, fill = clube)) +
   ) +
   theme_minimal()
 
+#Média de gols por partida do Flamengo nos anos de 2020 a 2024
+MediaGolsMarcadosFlamengo <- campeonato_brasileiro_gols %>%
+  inner_join(campeonato_brasileiro_full, by = c("partida_id" = "ID")) %>%
+  filter(year(data) %in% 2020:2024 & clube == "Flamengo") %>%
+  group_by(clube, partida_id, ano = year(data)) %>%
+  summarize(gols_por_partida = n(), .groups = "drop") %>%
+  group_by(clube, ano) %>%
+  summarize(mean_gols = round(mean(gols_por_partida),2), .groups = "drop") %>%
+  arrange(desc(mean_gols))
 
+#Gráfico em linha da Média de gols por partida do Flamengo nos anos de 2020 a 2024
+ggplot(data = MediaGolsMarcadosFlamengo, aes(x = factor(ano), y = mean_gols, group = 1)) + 
+  geom_line(color = "black", size = 1) +
+  geom_point(color = "red", size = 3) +
+  geom_text(aes(label = mean_gols), 
+            vjust = -1.0, 
+            size = 4) +
+  labs(
+    title = "Média de gols por partida do Flamengo",
+    subtitle = "Valores médios de gols por partida, de 2020 a 2024",
+    x = "Ano",
+    y = "Média de gols"
+  ) +
+  theme_minimal()
